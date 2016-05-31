0cf0eb0cf232793a7a9f3cbe5b4433ea7d4b66a0
viswesvar <viswesvar.11@gmail.com>
2016-05-31 19:00:19 +0100

    Test all buttons in the home page( About,Contacts) and in my page (About , home ,contacts)

---
+++ src/test/java/com/example/tests/Testallbuttoninhome.java
@@ -0,0 +1,77 @@
+package com.example.tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class Testallbuttoninhome {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new HtmlUnitDriver();
+    baseUrl = "http://develop-viswesvarsekar.rhcloud.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testAllbuttoninhome() throws Exception {
+    driver.get(baseUrl + "/index.html");
+    driver.findElement(By.linkText("Viswesvar Sekar 2152249")).click();
+    assertTrue(isElementPresent(By.linkText("Home")));
+    assertTrue(isElementPresent(By.linkText("About")));
+    assertTrue(isElementPresent(By.linkText("Contact")));
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
