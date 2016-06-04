f949aa2289d52efaa4f365d8f7392f2059c5ac8d
viswesvar <viswesvar.11@gmail.com>
2016-06-04 16:38:17 +0100

    Test Ended - final footer test (04-06-2016 ) 16:33

---
+++ src/test/java/com/example/tests/Finalendtest.java
@@ -0,0 +1,81 @@
+package com.example.tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
+
+public class Finalendtest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new HtmlUnitDriver();
+    baseUrl = "http://develop-viswesvarsekar.rhcloud.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testFinalend() throws Exception {
+    driver.get(baseUrl + "/index.html");
+    driver.findElement(By.cssSelector("p")).click();
+    assertEquals("Webpage made by [Viswesvar Sekar]", driver.findElement(By.cssSelector("#footer > p")).getText());
+    try {
+      assertEquals("Webpage made by [Viswesvar Sekar]", driver.findElement(By.cssSelector("#footer > p")).getText());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    assertTrue(isElementPresent(By.cssSelector("img[alt=\"Viswesvar Sekar\"]")));
+    assertEquals("Gurukiran Manjunath", driver.findElement(By.id("logoLink")).getText());
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
---
+++ src/test/java/com/example/tests/FinaltestFooter.java
@@ -0,0 +1,80 @@
+package com.example.tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class FinaltestFooter {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new HtmlUnitDriver();
+    baseUrl = "http://develop-viswesvarsekar.rhcloud.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testFinaltestFooter() throws Exception {
+    driver.get(baseUrl + "/index.html");
+    driver.findElement(By.linkText("Viswesvar Sekar 2152249")).click();
+    try {
+      assertEquals("Webpage made by [Viswesvar Sekar]", driver.findElement(By.cssSelector("#footer > p")).getText());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    assertEquals("Webpage made by [Viswesvar Sekar]", driver.findElement(By.cssSelector("#footer > p")).getText());
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
